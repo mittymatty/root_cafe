a2d90be2023321c33e10b72e80e918883dd06320
mittymatty <mittymattychickennugget@gmail.com>
2026-09-26 16:05:27 +1200

    Kitchen and Counter Switcher UI

--- scripts/globals/SignalHub.gd
+++ scripts/globals/SignalHub.gd
@@ -4,9 +4,18 @@ signal add_ingredient (ingredient : String, quantity : float)
 
 signal cup_capacity_changed
 
+signal enter_kitchen
+
+signal enter_counter
+
 func emit_add_ingredient (ingredient : String, quantity : float) -> void:
 	add_ingredient.emit(ingredient,quantity)
 
-
 func emit_cup_capacity_changed () -> void:
 	cup_capacity_changed.emit()
+
+func emit_enter_kitchen () -> void:
+	enter_kitchen.emit()
+
+func emit_enter_counter () -> void:
+	enter_counter.emit()
