8feb438de02884d3bfba135c4ed72d6854e72783
mittymatty <mittymattychickennugget@gmail.com>
2026-09-26 21:27:31 +1200

    Added Player Status Global + Organisation

--- scripts/counter.gd
+++ scripts/counter.gd
@@ -3,6 +3,15 @@ extends Node2D
 var ordertaken = false 
 var current_customer = 0
 
+@onready var customer_id: Label = $"OrderUI/BAR/Customer id"
+@onready var order_text: Label = $"OrderUI/BAR/Order text"
+@onready var takeorder: Button = $OrderUI/BAR/Takeorder
+@onready var placeholder: Button = $OrderUI/BAR/placeholder
+@onready var dialogue_text: Label = $OrderUI/Panel/DialogueText
+
+@onready var animal: Sprite2D = $Animal
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 var customers = [
 	{
 		"id": "Kiwi",
@@ -25,11 +34,11 @@ var customers = [
 ]
 
 func _ready() -> void:
-	$OrderUI/BAR/Takeorder.text = "Take order"
-	$OrderUI/BAR/Takeorder.disabled = false
+	takeorder.text = "Take order"
+	takeorder.disabled = false
 	
 	#disable the new customer button at the start and generate the first customer
-	$OrderUI/BAR/placeholder.disabled = true
+	placeholder.disabled = true
 	_on_placeholder_pressed()
 	
 	#update the order
@@ -40,17 +49,17 @@ func _ready() -> void:
 func update_order():
 	var customer = customers[current_customer]
 	
-	$"OrderUI/BAR/Customer id".text = customer["id"]
-	$"OrderUI/BAR/Order text".text = customer["drink"]
-	$"OrderUI/Panel/DialogueText".text = customer["dialogue"]
+	customer_id.text = customer["id"]
+	order_text.text = customer["drink"]
+	dialogue_text.text = customer["dialogue"]
 	print("Updated dialogue to: ", customer["dialogue"])
 	
 func _on_takeorder_pressed() -> void:
 	ordertaken = true
-	$OrderUI/BAR/Takeorder.text = "Order taken!\n\n ✓ Active order"
-	$OrderUI/BAR/Takeorder.disabled = true
+	takeorder.text = "Order taken!\n\n ✓ Active order"
+	takeorder.disabled = true
 	
-	$OrderUI/BAR/placeholder.disabled = false
+	placeholder.disabled = false
 
 func _on_placeholder_pressed() -> void:
 	var previous_customer = current_customer
@@ -58,19 +67,19 @@ func _on_placeholder_pressed() -> void:
 	while current_customer == previous_customer:
 		current_customer = randi_range(0, customers.size() - 1)
 	ordertaken = false
-	$OrderUI/BAR/Takeorder.text = "Take order"
-	$OrderUI/BAR/Takeorder.disabled = false
-	$OrderUI/BAR/placeholder.disabled = true
+	takeorder.text = "Take order"
+	takeorder.disabled = false
+	placeholder.disabled = true
 	#update dialouge 
 	update_order()
 	#play walk out animation
-	$"PLACEHOLDER FOR ANIMALS/AnimationPlayer".play("Walk_out")
-	await $"PLACEHOLDER FOR ANIMALS/AnimationPlayer".animation_finished
+	animation_player.play("Walk_out")
+	await animation_player.animation_finished
 	#after that swap the customer sprites
 	# $"PLACEHOLDER FOR ANIMALS".texture = load(customers[current_customer]["texture"])
 	#then play the walk in animation
-	$"PLACEHOLDER FOR ANIMALS/AnimationPlayer".play("Walk_in")
-	await $"PLACEHOLDER FOR ANIMALS/AnimationPlayer".animation_finished
+	animation_player.play("Walk_in")
+	await animation_player.animation_finished
 	#then lock the button for new customers
-	$OrderUI/BAR/placeholder.disabled = true
+	placeholder.disabled = true
 	
