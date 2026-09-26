cc911225f80a93edc0f29d19a64cc4797516d4a5
Mitchell Berry <supg293@gmail.com>
2026-09-26 18:20:10 +1200

    Added walk in and out animation for the customer placeholder

--- scripts/counter.gd
+++ scripts/counter.gd
@@ -7,27 +7,39 @@ var customers = [
 	{
 		"id": "Kiwi",
 		"drink": "Hot Chocolate",
-		"dialogue": "I'm feeling a bit chilly,\n I'd like a chocolatey one today please!"
+		"dialogue": "I'm feeling a bit chilly,\n I'd like a chocolatey one today please!",
+		#"texture": "example path"
 	},
 	{
 		"id": "Fennec Fox",
 		"drink": "cocoa",
-		"dialogue": "Just the usual.\n cocoa for me, thanks."
+		"dialogue": "Just the usual.\n cocoa for me, thanks.",
+		#"texture": "example path"
 	},
 	{
 		"id": "Mole",
 		"drink": "Coffee",
-		"dialogue": "Need something to keep me awake on the go.\n Dark and strong."
+		"dialogue": "Need something to keep me awake on the go.\n Dark and strong.",
+		#"texture": "example path"
 	}
 ]
 
 func _ready() -> void:
 	$OrderUI/BAR/Takeorder.text = "Take order"
-	$OrderUI/BAR/placeholder.disabled = true
-	update_order()
+	$OrderUI/BAR/Takeorder.disabled = false
 	
+	#disable the new customer button at the start and generate the first customer
+	$OrderUI/BAR/placeholder.disabled = true
+	_on_placeholder_pressed()
+	
+	#update the order
+	
+	update_order()
+
+
 func update_order():
 	var customer = customers[current_customer]
+	
 	$"OrderUI/BAR/Customer id".text = customer["id"]
 	$"OrderUI/BAR/Order text".text = customer["drink"]
 	$"OrderUI/Panel/DialogueText".text = customer["dialogue"]
@@ -45,11 +57,20 @@ func _on_placeholder_pressed() -> void:
 	
 	while current_customer == previous_customer:
 		current_customer = randi_range(0, customers.size() - 1)
-	
 	ordertaken = false
 	$OrderUI/BAR/Takeorder.text = "Take order"
 	$OrderUI/BAR/Takeorder.disabled = false
-	
+	$OrderUI/BAR/placeholder.disabled = true
+	#update dialouge 
+	update_order()
+	#play walk out animation
+	$"PLACEHOLDER FOR ANIMALS/AnimationPlayer".play("Walk_out")
+	await $"PLACEHOLDER FOR ANIMALS/AnimationPlayer".animation_finished
+	#after that swap the customer sprites
+	# $"PLACEHOLDER FOR ANIMALS".texture = load(customers[current_customer]["texture"])
+	#then play the walk in animation
+	$"PLACEHOLDER FOR ANIMALS/AnimationPlayer".play("Walk_in")
+	await $"PLACEHOLDER FOR ANIMALS/AnimationPlayer".animation_finished
+	#then lock the button for new customers
 	$OrderUI/BAR/placeholder.disabled = true
 	
-	update_order()
